3eef5f1771bd98f8429775d54f4a5afce9173afa
drazdyakonov <daniil.razdyakonov@vismart.biz>
2016-09-30 11:28:42 +0300

    Iri of 'http://xmlns.com/foaf/0.1/Organization' was fixed

--- src/ontodia/customization/templates/defaultTemplates.ts
+++ src/ontodia/customization/templates/defaultTemplates.ts
@@ -41,7 +41,7 @@ export const DefaultTemplateBundle: TemplateResolver[] = [
     types => {
         if (types.indexOf('http://schema.org/Organization') !== -1 ||
             types.indexOf('http://dbpedia.org/ontology/Organisation') !== -1 ||
-            types.indexOf('http://xmlns.com/foaf/0.1/Organisation') !== -1
+            types.indexOf('http://xmlns.com/foaf/0.1/Organization') !== -1
         ) {
             return OrganizationTemplate;
         } else {
