7721fd8ee4168d4b99935469434657195b9ee189
drazdyakonov <daniil.razdyakonov@vismart.biz>
2016-09-30 11:28:42 +0300

    Iri of 'http://xmlns.com/foaf/0.1/Organization' was fixed

--- src/ontodia/customization/defaultTypeStyles.ts
+++ src/ontodia/customization/defaultTypeStyles.ts
@@ -26,7 +26,7 @@ export const DefaultTypeStyleBundle: TypeStyleResolver[] = [
     types => {
         if (types.indexOf('http://schema.org/Organization') !== -1 ||
             types.indexOf('http://dbpedia.org/ontology/Organisation') !== -1 ||
-            types.indexOf('http://xmlns.com/foaf/0.1/Organisation') !== -1) {
+            types.indexOf('http://xmlns.com/foaf/0.1/Organization') !== -1) {
             return {color: '#77ca98', icon: 'ontodia-organization-icon'};
         }
     },
