a2a9a24588d3c18258b815e0c49f4f37ba97931d
Olga Belyaeva <olga.belyaeva@vismart.biz>
2017-12-20 20:07:28 +0300

    WIP Calculate size of group

--- src/ontodia/diagram/embeddedLayer.tsx
+++ src/ontodia/diagram/embeddedLayer.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 
 import { DiagramView } from './view';
-import { Paper } from './paper';
+import { Paper, Cell } from './paper';
 import { Element } from './elements';
 import { ElementLayer } from './elementLayer';
 import { EventObserver } from '../viewUtils/events';
+import {
+    LayoutNode,
+    LayoutLink,
+    forceLayout,
+    padded,
+    removeOverlaps,
+    translateToPositiveQuadrant,
+} from '../viewUtils/layout';
+import { getContentFittingBox } from './paperArea';
 
 export interface Props {
     view: DiagramView;
@@ -16,42 +25,134 @@ export class EmbeddedLayer extends React.Component<Props, void> {
     private layer: HTMLDivElement;
     private elements: Element[] = [];
     private offset: { x: number; y: number } = {x: 0, y: 0};
+    private paperBox: {
+        x: number; y: number; width: number; height: number;
+    } = {x: 0, y: 0, width: 0, height: 0};
+
+    private movingElement: Element;
 
     componentDidMount() {
-        const {view, element} = this.props;
-        const {id, template} = element;
+        const {element} = this.props;
+
+        document.addEventListener('mouseup', this.onMouseUp);
 
         this.listener.listenTo(element, 'change', () => {
             for (const changedKey in element.changed) {
                 if (!element.changed.hasOwnProperty(changedKey)) { continue; }
 
                 if (changedKey === 'position') {
-                    this.setPositions();
+                    this.setOffset(this.movingElement === undefined);
                     this.updateAll();
                 }
             }
         });
 
-        view.loadEmbeddedElements(id, template.id).then(res => {
-            const elements = Object.keys(res).map(key => view.model.createElement(res[key]));
-
-            view.model.requestElementData(elements);
-            view.model.requestLinksOfType();
-
-            this.elements = elements;
-
-            this.setPositions();
-            this.updateAll();
-        });
+        this.loadEmbeddedElements();
     }
 
     componentWillUnmount() {
+        document.removeEventListener('mouseup', this.onMouseUp);
         this.listener.stopListening();
+    }
+
+    private onMouseUp = () => {
+        this.movingElement = undefined;
+    }
+
+    private loadEmbeddedElements = () => {
+        const {view, element} = this.props;
+        const {id, template} = element;
+
+        view.loadEmbeddedElements(id, template.id).then(res => {
+            this.elements = Object.keys(res).map(key => view.model.createElement(res[key]));
+            this.elements.forEach(this.listenElement);
+
+            view.model.requestElementData(this.elements);
+            view.model.requestLinksOfType().then(() => {
+                this.forceLayout();
+                this.setOffset();
+
+                this.paperBox = getContentFittingBox(this.elements, []);
+                this.props.element.trigger('state:loaded');
+
+                this.updateAll();
+            });
+        });
+    }
+
+    private listenElement = (element: Element) => {
+        this.listener.listenTo(element, 'change', () => {
+            if (!this.movingElement || this.movingElement.id !== element.id) { return; }
+
+            for (const changedKey in element.changed) {
+                if (!element.changed.hasOwnProperty(changedKey)) { continue; }
+
+                if (changedKey === 'position') {
+                    this.onChangePosition();
+                }
+            }
+        });
+    }
+
+    private onChangePosition = () => {
+        this.paperBox = getContentFittingBox(this.elements, []);
+
+        const {x, y} = this.paperBox;
+        const {offsetLeft, offsetTop} = this.layer;
+
+        this.props.element.set('position', {x: x - offsetLeft, y: y - offsetTop});
+        this.props.element.trigger('state:loaded');
+
+        this.updateAll();
     }
 
     private updateAll = () => this.forceUpdate();
 
-    private setPositions = () => {
+    private forceLayout = () => {
+        const {view} = this.props;
+
+        const nodeById: { [id: string]: LayoutNode } = {};
+        const nodes: LayoutNode[] = [];
+        for (const element of this.elements) {
+            const size = element.get('size');
+            const position = element.get('position');
+
+            const node: LayoutNode = {
+                id: element.id,
+                x: position.x,
+                y: position.y,
+                width: size.width,
+                height: size.height,
+            };
+            nodeById[element.id] = node;
+            nodes.push(node);
+        }
+
+        const links: LayoutLink[] = [];
+        for (const link of view.model.links) {
+            if (!view.model.isSourceAndTargetVisible(link)) { continue; }
+
+            const source = view.model.sourceOf(link);
+            const target = view.model.targetOf(link);
+
+            const sourceNode = nodeById[source.id];
+            const targetNode = nodeById[target.id];
+
+            if (!sourceNode || !targetNode) { continue; }
+
+            links.push({source: sourceNode, target: targetNode});
+        }
+
+        forceLayout({nodes, links, preferredLinkLength: 200});
+        padded(nodes, {x: 10, y: 10}, () => removeOverlaps(nodes));
+        translateToPositiveQuadrant({nodes, padding: {x: 0, y: 0}});
+
+        for (const node of nodes) {
+            view.model.getElement(node.id).position(node.x, node.y);
+        }
+    }
+
+    private setOffset = (updatePositions = true) => {
         const {x: elementX, y: elementY} = this.props.element.get('position');
         const {offsetLeft, offsetTop} = this.layer;
         const {x: offsetX, y: offsetY} = this.offset;
@@ -59,20 +160,35 @@ export class EmbeddedLayer extends React.Component<Props, void> {
         const newOffsetX = elementX + offsetLeft;
         const newOffsetY = elementY + offsetTop;
 
-        const diffX = newOffsetX - offsetX;
-        const diffY = newOffsetY - offsetY;
-
-        this.elements.forEach(element => {
-            const {x, y} = element.get('position') || {x: 0, y: 0};
-            const newPosition = {x: x + diffX, y: y + diffY};
-            element.set('position', newPosition);
-        });
         this.offset = {x: newOffsetX, y: newOffsetY};
+
+        if (updatePositions) {
+            const diffX = newOffsetX - offsetX;
+            const diffY = newOffsetY - offsetY;
+
+            this.elements.forEach(element => {
+                const {x, y} = element.get('position') || {x: 0, y: 0};
+                const newPosition = {x: x + diffX, y: y + diffY};
+                element.set('position', newPosition);
+            });
+        }
+    }
+
+    private onPaperPointerDown = (e: React.MouseEvent<HTMLElement>, cell: Cell | undefined) => {
+        if (e.button !== 0 /* left mouse button */) {
+            return;
+        }
+
+        if (cell && cell instanceof Element) {
+            e.preventDefault();
+            this.movingElement = cell;
+        }
     }
 
     render() {
         const {view, element} = this.props;
         const {x: offsetX, y: offsetY} = this.offset;
+        const {width, height} = this.paperBox;
 
         const style = {
             position: 'absolute', left: -offsetX, top: -offsetY,
@@ -81,14 +197,14 @@ export class EmbeddedLayer extends React.Component<Props, void> {
         return (
             <div className="ontodia-embedded-layer" ref={layer => this.layer = layer}>
                 <Paper view={view}
-                       width={400}
-                       height={400}
+                       width={width}
+                       height={height}
                        originX={-offsetX}
                        originY={-offsetY}
                        scale={1}
                        paddingX={0}
                        paddingY={0}
-                       onPointerDown={() => {}}
+                       onPointerDown={this.onPaperPointerDown}
                        group={element.id}>
                     <ElementLayer view={view} group={element.id} style={style} />
                 </Paper>
--- src/ontodia/diagram/paperArea.tsx
+++ src/ontodia/diagram/paperArea.tsx
@@ -277,35 +277,8 @@ export class PaperArea extends React.Component<Props, State> {
 
     /** Returns bounding box of paper content in paper coordinates. */
     getContentFittingBox() {
-        let minX = Infinity, minY = Infinity;
-        let maxX = -Infinity, maxY = -Infinity;
-
-        const model = this.props.view.model;
-        for (const element of model.elements) {
-            const {x, y} = element.get('position');
-            const size = element.get('size') || {width: 0, height: 0};
-            minX = Math.min(minX, x);
-            minY = Math.min(minY, y);
-            maxX = Math.max(maxX, x + size.width);
-            maxY = Math.max(maxY, y + size.height);
-        }
-
-        for (const link of model.links) {
-            const vertices = link.vertices || [];
-            for (const {x, y} of vertices) {
-                minX = Math.min(minX, x);
-                minY = Math.min(minY, y);
-                maxX = Math.max(maxX, x);
-                maxY = Math.max(maxY, y);
-            }
-        }
-
-        return {
-            x: Number.isFinite(minX) ? minX : 0,
-            y: Number.isFinite(minY) ? minY : 0,
-            width: Number.isFinite(minX) && Number.isFinite(maxX) ? (maxX - minX) : 0,
-            height: Number.isFinite(minY) && Number.isFinite(maxY) ? (maxY - minY) : 0,
-        };
+        const {elements, links} = this.props.view.model;
+        return getContentFittingBox(elements, links);
     }
 
     /** Returns paper size in paper coordinates. */
@@ -806,5 +779,38 @@ function clientCoordsFor(container: HTMLElement, e: MouseEvent) {
     return {
         x: e.offsetX + (targetBox.left - containerBox.left),
         y: e.offsetY + (targetBox.top - containerBox.top),
+    };
+}
+
+export function getContentFittingBox(
+    elements: Element[], links: Link[]
+): { x: number; y: number; width: number; height: number; } {
+    let minX = Infinity, minY = Infinity;
+    let maxX = -Infinity, maxY = -Infinity;
+
+    for (const element of elements) {
+        const {x, y} = element.get('position');
+        const size = element.get('size') || {width: 0, height: 0};
+        minX = Math.min(minX, x);
+        minY = Math.min(minY, y);
+        maxX = Math.max(maxX, x + size.width);
+        maxY = Math.max(maxY, y + size.height);
+    }
+
+    for (const link of links) {
+        const vertices = link.vertices || [];
+        for (const {x, y} of vertices) {
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+    }
+
+    return {
+        x: Number.isFinite(minX) ? minX : 0,
+        y: Number.isFinite(minY) ? minY : 0,
+        width: Number.isFinite(minX) && Number.isFinite(maxX) ? (maxX - minX) : 0,
+        height: Number.isFinite(minY) && Number.isFinite(maxY) ? (maxY - minY) : 0,
     };
 }
