ef8a6b2dd24b80f41fb2d82ddf87ae1c8f2e553e
Olga Belyaeva <olga.belyaeva@vismart.biz>
2017-12-22 14:51:39 +0300

    WIP Add groups styles

--- src/ontodia/customization/templates/group.tsx
+++ src/ontodia/customization/templates/group.tsx
@@ -4,11 +4,33 @@ import { TemplateProps } from '../props';
 
 export class GroupTemplate extends React.Component<TemplateProps, void> {
     render() {
-        const {label, embeddedLayer} = this.props;
+        const {label, icon, types, color, isExpanded, embeddedLayer} = this.props;
+
         return (
-            <div style={{background: '#ddd', padding: '5px 10px', overflow: 'hidden'}}>
-                {label}
-                {embeddedLayer}
+            <div className='ontodia-group-template'>
+                <div className='ontodia-default-template ontodia-group-template__wrap' style={{
+                    backgroundColor: color,
+                    borderColor: color,
+                }}>
+                    <div className='ontodia-default-template_type-line' title={label}>
+                        <div className={`${icon} ontodia-default-template_type-line__icon`} />
+                        <div title={types} className='ontodia-default-template_type-line_text-container'>
+                            <div className='ontodia-default-template_type-line_text-container__text'>
+                                {types}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='ontodia-default-template_body ontodia-group-template__body' style={{borderColor: color}}>
+                    <span className='ontodia-default-template_body__label' title={label}>
+                        {label}
+                    </span>
+                        {
+                            isExpanded ? (
+                                <div className='ontodia-group-template__embedded-layer'>{embeddedLayer}</div>
+                            ) : null
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
