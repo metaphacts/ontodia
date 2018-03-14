f5dd37d0df5755d86bc4c91418ae89e322be0d41
Olga Belyaeva <olga.belyaeva@vismart.biz>
2018-03-14 19:37:59 +0300

    Polyfill the remove method in IE

--- src/ontodia/viewUtils/polyfills.ts
+++ src/ontodia/viewUtils/polyfills.ts
@@ -70,3 +70,21 @@ if (!Array.prototype.find) {
     };
     Object.defineProperty(Array.prototype, 'find', {value: arrayFind});
 }
+
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove(this: any) {
+                if (this.parentNode !== null)
+                    this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
