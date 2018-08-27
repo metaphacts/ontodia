fc50089780eb9c506ae9ab04e6ed3c2dbde474df
Daniel <daniil.razdyakonov@vismart.biz>
2018-08-27 13:51:47 +0300

    Refactor recursive layout functions. (#138)

--- src/ontodia/viewUtils/layout.ts
+++ src/ontodia/viewUtils/layout.ts
@@ -1,4 +1,10 @@
 import * as cola from 'webcola';
+import { DiagramModel } from '../diagram/model';
+import { boundsOf, Vector, computeGrouping } from '../diagram/geometry';
+import { Element } from '../diagram/elements';
+import { ElementIri } from '../data/model';
+import { EventObserver } from './events';
+import { DiagramView } from '../diagram/view';
 
 export interface LayoutNode {
     id?: string;
@@ -121,4 +127,148 @@ export function padded(
             node.height -= 2 * padding.y;
         }
     }
+}
+
+export function recursiveLayout(params: {
+    model: DiagramModel;
+    grouping: Map<string, Element[]>;
+    group?: string;
+    layoutFunction: (nodes: LayoutNode[], links: LayoutLink[], model: DiagramModel) => void;
+}) {
+    const {model, grouping, group, layoutFunction} = params;
+    const elements = group
+        ? grouping.get(group)
+        : model.elements.filter(el => el.group === undefined);
+
+    for (const element of elements) {
+        if (grouping.has(element.id)) {
+            recursiveLayout({model, grouping, group: element.id, layoutFunction});
+        }
+    }
+
+    const nodes: LayoutNode[] = [];
+    const nodeById: { [id: string]: LayoutNode } = {};
+    for (const element of elements) {
+        const {x, y, width, height} = boundsOf(element);
+        const node: LayoutNode = {id: element.id, x, y, width, height};
+        nodeById[element.id] = node;
+        nodes.push(node);
+    }
+
+    const links: LayoutLink[] = [];
+    for (const link of model.links) {
+        if (!model.isSourceAndTargetVisible(link)) {
+            continue;
+        }
+        const source = model.sourceOf(link);
+        const target = model.targetOf(link);
+
+        const sourceNode = nodeById[source.id];
+        const targetNode = nodeById[target.id];
+
+        if (sourceNode && targetNode) {
+            links.push({source: sourceNode, target: targetNode});
+        }
+    }
+
+    layoutFunction(nodes, links, model);
+
+    for (const node of nodes) {
+        const element = model.getElement(node.id);
+        element.setPosition({x: node.x, y: node.y});
+    }
+}
+
+export function placeElementsAround(params: {
+    model: DiagramModel;
+    elements: ReadonlyArray<Element>;
+    prefferedLinksLength: number;
+    targetElement: Element;
+    startAngle?: number;
+}) {
+    const {model, elements, targetElement, prefferedLinksLength} = params;
+    const targetElementBounds = boundsOf(targetElement);
+    const targetPosition: Vector = {
+        x: targetElementBounds.x + targetElementBounds.width / 2,
+        y: targetElementBounds.y + targetElementBounds.height / 2,
+    };
+    let outgoingAngle = 0;
+    if (targetElement.links.length > 0) {
+        let xSum = 0;
+        let ySum = 0;
+        for (const link of targetElement.links) {
+            const linkSource = model.sourceOf(link);
+            const source = linkSource !== targetElement ? linkSource : model.targetOf(link);
+            xSum += source.position.x + source.size.width / 2;
+            ySum += source.position.y + source.size.height / 2;
+        }
+        const averageSourcePosition: Vector = {
+            x: xSum / targetElement.links.length, y: ySum / targetElement.links.length,
+        };
+        const vectorDiff: Vector = {
+            x: targetPosition.x - averageSourcePosition.x, y: targetPosition.y - averageSourcePosition.y,
+        };
+        if (vectorDiff.x !== 0 || vectorDiff.y !== 0) {
+            outgoingAngle = Math.atan2(vectorDiff.y, vectorDiff.x);
+        }
+    }
+
+    const step = Math.min(Math.PI / elements.length, Math.PI / 6);
+    const elementsSteck: Element[]  = [].concat(elements);
+
+    const placeElementFromSteck = (curAngle: number, element: Element) => {
+        if (element) {
+            const size = element.size;
+            element.setPosition({
+                x: targetPosition.x + prefferedLinksLength * Math.cos(curAngle) - size.width / 2,
+                y: targetPosition.y + prefferedLinksLength * Math.sin(curAngle) - size.height / 2,
+            });
+        }
+    };
+
+    const isOddLength = elementsSteck.length % 2 === 0;
+    if (isOddLength) {
+        for (let angle = step / 2; elementsSteck.length > 0; angle += step) {
+            placeElementFromSteck(outgoingAngle - angle, elementsSteck.pop());
+            placeElementFromSteck(outgoingAngle + angle, elementsSteck.pop());
+        }
+    } else {
+        placeElementFromSteck(outgoingAngle, elementsSteck.pop());
+        for (let angle = step; elementsSteck.length > 0; angle += step) {
+            placeElementFromSteck(outgoingAngle - angle, elementsSteck.pop());
+            placeElementFromSteck(outgoingAngle + angle, elementsSteck.pop());
+        }
+    }
+
+    return new Promise(resolve => {
+        const listener = new EventObserver();
+        listener.listen(model.events, 'changeCells', () => {
+            listener.stopListening();
+
+            const grouping = computeGrouping(model.elements);
+            recursiveRemoveOverlaps({
+                model,
+                grouping,
+                padding: { x: 15, y: 15 },
+            });
+            resolve();
+        });
+    });
+}
+
+export function recursiveRemoveOverlaps(params: {
+    model: DiagramModel;
+    grouping: Map<string, Element[]>;
+    padding?: Vector;
+    group?: string;
+}) {
+    const {padding, model, grouping, group} = params;
+    recursiveLayout({
+        model,
+        grouping,
+        group,
+        layoutFunction: (nodes) => {
+            padded(nodes, padding, () => removeOverlaps(nodes));
+        },
+    });
 }
