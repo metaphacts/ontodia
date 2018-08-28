f5d3adf6edba7b36ef54496b5167106322321c82
Daniel <daniil.razdyakonov@vismart.biz>
2018-08-28 14:50:24 +0300

    Extract functions 'makeGraphItems' and 'makeLayout' from GraphBuilder to API. (#141)

--- src/ontodia/data/sparql/graphBuilder.ts
+++ src/ontodia/data/sparql/graphBuilder.ts
@@ -21,7 +21,7 @@ export class GraphBuilder {
     }> {
         return this.dataProvider.elementInfo({elementIds: graph.elementIds}).then(elementsInfo => ({
             preloadedElements: elementsInfo,
-            layoutData: this.getLayout(graph.elementIds, graph.links),
+            layoutData: makeLayout(graph.elementIds, graph.links),
         }));
     }
 
@@ -29,7 +29,7 @@ export class GraphBuilder {
         preloadedElements: Dictionary<ElementModel>;
         layoutData: LayoutData;
     }> {
-        const {elementIds, links} = this.getGraphElements(graph);
+        const {elementIds, links} = makeGraphItems(graph);
         return this.createGraph({elementIds, links});
     }
 
@@ -39,62 +39,62 @@ export class GraphBuilder {
     }> {
         return parseTurtleText(graph).then(triples => this.getGraphFromRDFGraph(triples));
     }
-
-    private getGraphElements(response: Triple[]): {
-        elementIds: ElementIri[];
-        links: LinkModel[];
-    } {
-        const elements: Dictionary<boolean> = {};
-        const links: LinkModel[] = [];
-
-        for (const {subject, predicate, object} of response) {
-            if (subject.type === 'uri' && !elements[subject.value]) {
-                elements[subject.value] = true;
-            }
-
-            if (object.type === 'uri' && !elements[object.value]) {
-                elements[object.value] = true;
-            }
-
-            if (subject.type === 'uri' && object.type === 'uri') {
-                links.push({
-                    linkTypeId: predicate.value as LinkTypeIri,
-                    sourceId: subject.value as ElementIri,
-                    targetId: object.value as ElementIri,
-                });
-            }
-        }
-        return {elementIds: Object.keys(elements) as ElementIri[], links};
-    }
-
-    private getLayout(elementsIds: ElementIri[], linksInfo: LinkModel[]): LayoutData {
-        const rows = Math.ceil(Math.sqrt(elementsIds.length));
-        const grid = uniformGrid({rows, cellSize: {x: GREED_STEP, y: GREED_STEP}});
-
-        const layoutElements: LayoutCell[] = elementsIds.map<LayoutElement>((id, index) => {
-            const {x, y} = grid(index);
-            return {type: 'element', id: `element_${generate64BitID()}`, iri: id, position: {x, y}};
-        });
-
-        const layoutElementsMap: {[iri: string]: LayoutCell} = keyBy(layoutElements, 'iri');
-        const layoutLinks: LayoutLink[] = [];
-
-        linksInfo.forEach((link, index) => {
-            const source = layoutElementsMap[link.sourceId];
-            const target = layoutElementsMap[link.targetId];
-
-            if (!source || !target) { return; }
-
-            layoutLinks.push({
-                type: 'link',
-                id: `link_${generate64BitID()}`,
-                typeId: link.linkTypeId,
-                source: {id: source.id},
-                target: {id: target.id},
-            })
-        });
-        return {cells: layoutElements.concat(layoutLinks)};
-    }
 }
 
 export default GraphBuilder;
+
+export function makeGraphItems(response: ReadonlyArray<Triple>): {
+    elementIds: ElementIri[];
+    links: LinkModel[];
+} {
+    const elements: Dictionary<boolean> = {};
+    const links: LinkModel[] = [];
+
+    for (const {subject, predicate, object} of response) {
+        if (subject.type === 'uri' && !elements[subject.value]) {
+            elements[subject.value] = true;
+        }
+
+        if (object.type === 'uri' && !elements[object.value]) {
+            elements[object.value] = true;
+        }
+
+        if (subject.type === 'uri' && object.type === 'uri') {
+            links.push({
+                linkTypeId: predicate.value as LinkTypeIri,
+                sourceId: subject.value as ElementIri,
+                targetId: object.value as ElementIri,
+            });
+        }
+    }
+    return {elementIds: Object.keys(elements) as ElementIri[], links};
+}
+
+export function makeLayout(elementsIds: ReadonlyArray<ElementIri>, linksInfo: ReadonlyArray<LinkModel>): LayoutData {
+    const rows = Math.ceil(Math.sqrt(elementsIds.length));
+    const grid = uniformGrid({rows, cellSize: {x: GREED_STEP, y: GREED_STEP}});
+
+    const layoutElements: LayoutCell[] = elementsIds.map<LayoutElement>((id, index) => {
+        const {x, y} = grid(index);
+        return {type: 'element', id: `element_${generate64BitID()}`, iri: id, position: {x, y}};
+    });
+
+    const layoutElementsMap: {[iri: string]: LayoutCell} = keyBy(layoutElements, 'iri');
+    const layoutLinks: LayoutLink[] = [];
+
+    linksInfo.forEach((link, index) => {
+        const source = layoutElementsMap[link.sourceId];
+        const target = layoutElementsMap[link.targetId];
+
+        if (!source || !target) { return; }
+
+        layoutLinks.push({
+            type: 'link',
+            id: `link_${generate64BitID()}`,
+            typeId: link.linkTypeId,
+            source: {id: source.id},
+            target: {id: target.id},
+        });
+    });
+    return {cells: layoutElements.concat(layoutLinks)};
+}
