25b9c2a5985a21ba08fe90a22e764e0d72bde66c
Daniel Razdiakonov <daniil.razdyakonov@vismart.biz>
2018-11-15 18:27:05 +0300

    Add URL prefix to id's and remove "@base" from contex. (#170)

--- src/ontodia/data/schema.ts
+++ src/ontodia/data/schema.ts
@@ -7,10 +7,11 @@ export const DIAGRAM_CONTEXT_URL_V1 = 'https://ontodia.org/context/v1.json';
 
 export const PLACEHOLDER_ELEMENT_TYPE = 'http://ontodia.org/NewEntity' as ElementTypeIri;
 export const PLACEHOLDER_LINK_TYPE = 'http://ontodia.org/NewLink' as LinkTypeIri;
+const ONTODIA_ID_URL_PREFIX = 'http://ontodia.org/data/';
 
 export namespace GenerateID {
-    export function forElement() { return 'e_' + generate128BitID(); }
-    export function forLink() { return 'l_' + generate128BitID(); }
+    export function forElement() { return `${ONTODIA_ID_URL_PREFIX}e_${generate128BitID()}`; }
+    export function forLink() { return `${ONTODIA_ID_URL_PREFIX}l_${generate128BitID()}`; }
     export function forNewEntity(): ElementIri {
         return `http://ontodia.org/newEntity_${generate128BitID()}` as ElementIri;
     }
